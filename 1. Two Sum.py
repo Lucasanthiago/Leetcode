c38f77453b00d53f51ae43732741fede08a684a8
Lucas <santoslblucas@gmail.com>
2025-02-04 00:07:03 -0300

    two sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,3 +1,4 @@
+from typing import List
 
 #1 Solution(very bad)
 class Solution1:
@@ -10,7 +11,7 @@ class Solution1:
 
 
 #2 Solution
-class Solution:
+class Solution2:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         for x in range(len(nums)):
@@ -20,7 +21,20 @@ class Solution:
 
 
 #3 Solution
-class Solution:
+class Solution3:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+            if compliment in hashmap:
+                return [i,hashmap[compliment]]
+            hashmap[nums[i]] = i
+        return []
 
 
+nums = [2, 7, 11, 15]
+target = 9
+
+solution = Solution1()
+result = solution.twoSum(nums, target)
+print(result)
