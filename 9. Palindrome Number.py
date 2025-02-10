ceaabc720347c7c61ff796c71ac89519b307f891
Lucas <santoslblucas@gmail.com>
2025-02-10 14:04:59 -0300

    14 e 13

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -32,5 +32,24 @@ class Solution2:
         return True
 
 
-solution = Solution2()
+class Solution3:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
+
+
+
+
+
+solution = Solution3()
 print(solution.isPalindrome(1221))
