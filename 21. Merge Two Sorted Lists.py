2d711cff6379c27f4eab6b3330e025ae625c655b
Lucas <santoslblucas@gmail.com>
2025-02-15 18:49:23 -0300

    217

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+
+
+solution = Solution()
+result = solution.mergeTwoLists(list1=None, list2=None)
+print(result.val)
+
